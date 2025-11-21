b988815ddb55809ec7e3e000defe49905dae1a86
ahwagner1 <125780429+ahwagner1@users.noreply.github.com>
2025-11-21 10:40:34 -0900

    writing up reasoning to better understand the manual i'm reading

--- c/src/include/instructions.h
+++ c/src/include/instructions.h
@@ -1,6 +1,58 @@
 #ifndef INSTRUCTION_H
 #define INSTRUCTION_H
 
+/*
+    Need to rethink how this is structured, in ARM7 only data processing instructions
+    contain traditional set opcodes. Everything else has it's own opcode, but those are
+    variable in length. One thing that is constant are the condition fields. Bits 31:28 
+    are always the condition field from the CPSR register for this ISA (ARM7TDMI-S)
+
+    Essentially the instructions with set opcodes are:
+        AND = 0b0000
+        EOR = 0b0001
+        SUB = 0b0010
+        RSB = 0b0011
+        ADD = 0b0100
+        ADC = 0b0101
+        SBC = 0b0110
+        RSC = 0b0111
+        TST = 0b1000
+        TEQ = 0b1001
+        CMP = 0b1010
+        CMN = 0b1011
+        ORR = 0b1100
+        MOV = 0b1101
+        BIC = 0b1110
+        MVN = 0b1111
+
+    Bits 27:... are how we determine the other instructions, it's probably smart to start
+    with the higher opcodes and work our way shorter
+    For example: if bits 27:26 == 0b00, then the instruction is a data processing type
+                 at the same time, 6 other instructions also have 27:26 as 0b00
+
+    There is one special form we want to be away of
+    if 27:24 == 0b1111 this signifies a software interrupt
+*/
+
+typedef enum {
+    AND = 0,
+    EOR,
+    SUB,
+    RSB,
+    ADD,
+    ADC,
+    SBC,
+    RSC,
+    TST,
+    TEQ,
+    CMP,
+    CMN,
+    ORR,
+    MOV,
+    BIC,
+    MNV
+} e_data_processing_opcodes;
+
 // need to redo this to match up the correct op code values
 // for example AND == 0b000
 // EOR == 0b0001
@@ -39,6 +91,9 @@ typedef enum {
     SWP,     // swap register with memory
     TEQ,     // test bitwise equality
     TST      // test bits
-} e_opcodes;
+} e_instructions;
+
+uint8_t extract_condition_field(uint32_t instruction);
+e_instructions determine_instruction(uint32_t instruction);
 
 #endif /* INSTRUCTION_H */
