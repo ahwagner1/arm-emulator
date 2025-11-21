f8b7ae75dbd5f322bdcd8031ed41992cba9c4500
ahwagner1 <125780429+ahwagner1@users.noreply.github.com>
2025-11-21 13:14:24 -0900

    setting up functions for determining instruction type

--- c/src/instructions.c
+++ c/src/instructions.c
@@ -1,17 +1,96 @@
 #include "include/instructions.h"
+#include "include/reg.h"
 
 uint8_t extract_condition_field(uint32_t instruction) {
     return instruction >> 28;
 }
 
-e_instructions determine_instruction(uint32_t instruction) {
-    // not sure the best way to extract the instruction type other than going from 
-    // longest to shortest opcode
-
-    uint8_t condition = extract_condition_field(instruction);
+void determine_instruction(uint32_t instruction) {
+    // not the prettiest but should get the job done
     
-    // easier to "discard" the condition flags for now
-    // which leaves 4 zeros on the end of the instruction now
-    instruction = instruction << 4;
+    uint8_t first_three = instruction & 0x0E000000; 
+    switch (first_three) {
+        case 0:
+            // data processing, multiply, or single data swap
+            // check bits 7:4 to determine if multiply/swap or data processing
+            if (instruction & 0x00000090) == 0x00000090) {
+                // bits 7:4 == 0b1001, so either multiply/swap
+                // check the bit 24, if 1 then single data swap
+                if ((instruction & 0x01000000) == 0x01000000)
+                    single_data_swap(instruction);
+                else
+                    multiply(instruction);
+            }
+            else {
+                data_processing(instruction); // not yet defined
+            }
+            break;
+        case 2: // intentional fall through to three might work here
+        case 3:
+            // undefined or single data transfer
+            // single data transfer can either be 0b010 or 0b011
+            // hence why we fall through to this conditional
+            // assuming no undefined things so far
+            single_data_transfer(instruction);
+            break;
+        case 4:
+            // block data transfer
+            block_data_transfer(instruction);
+            break;
+        case 5:
+            // branch
+            branch(instruction);
+            break;
+        case 6:
+            // coprocesser data transfer
+            coproc_data_transfer(instruction);
+            break;
+        case 7:
+            // coprocessor data operation or coprocessor register transfer
+            // or interrupt
+            if ((instruction & 0x0F000000) == 0x0F000000)
+                // software interrupt
+                interrupt();
+            else
+                coproc_data_op(instruction);
+            break;
+        default:
+            printf("Unknown instruction");
+    }
+}
+
+void single_data_swap(uint32_t instruction) {
+
+}
+
+void multiply(uint32_t instruction) {
+
+}
+
+void data_processing(uint32_t instruction) {
+    // extract opcode, run switch statement on enum values, setup those functions
+}
+
+void single_data_transfer(uint32_t instruction) {
+
+}
+
+void block_data_transfer(uint32_t instruction) {
+
+}
+
+void branch(uint32_t instruction) {
+
+}
+
+void coproc_data_transfer(uint32_t instruction) {
+
+}
+
+void coproc_data_op(uint32_t instruction) {
+
+}
+
+void interrupt() {
 
 }
