6133841ea0dab5d3b4b3806593134ce820add7d8
ahwagner1 <125780429+ahwagner1@users.noreply.github.com>
2025-11-21 11:15:09 -0900

    updating .c to match .h so far

--- c/src/instructions.c
+++ c/src/instructions.c
@@ -1,1 +1,17 @@
 #include "include/instructions.h"
+
+uint8_t extract_condition_field(uint32_t instruction) {
+    return instruction >> 28;
+}
+
+e_instructions determine_instruction(uint32_t instruction) {
+    // not sure the best way to extract the instruction type other than going from 
+    // longest to shortest opcode
+
+    uint8_t condition = extract_condition_field(instruction);
+    
+    // easier to "discard" the condition flags for now
+    // which leaves 4 zeros on the end of the instruction now
+    instruction = instruction << 4;
+
+}
